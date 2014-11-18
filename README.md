57bea6d9e0c118e3a9e53b694a293355f83269a3
rzymek <rzymek@gmail.com>
2014-11-18 10:43:40 +0100

    refactoring

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-# Moment v2.8.1 for Meteor [![Build Status](https://travis-ci.org/acreeger/meteor-moment.svg?branch=master)](https://travis-ci.org/acreeger/meteor-moment)
+# Moment.js for Meteor
 
 [Moment.js](http://momentjs.com/), a JavaScript date library for parsing, validating, manipulating, and formatting dates, packaged for Meteor.
 
 Installation
 -------------
 
-For Meteor v0.9.0 and above:
-`meteor add mrt:moment`
-
-For Meteor versions below v0.9.0 using Meteorite/AtmosphereJS:
-`mrt add moment`
+    meteor add rzymek:moment
 
 Usage
 -------------
--- package.js
+++ package.js
@@ -1,25 +1,13 @@
 Package.describe({
-  name: 'rzymek:moment-pl',
-  summary: "Moment.js, a JavaScript date library for dates, packaged for Meteor. See http://momentjs.com.",
-  version: "2.8.3_2",
-  git: "https://github.com/rzymek/meteor-moment.git"
+    name: 'rzymek:moment',
+    summary: "Moment.js, a JavaScript date library for dates, packaged for Meteor. See http://momentjs.com.",
+    version: "2.8.3_3",
+    git: "https://github.com/rzymek/meteor-moment.git"
 });
 
-Package.onUse(function (api) {
-  api.export('moment');
-  api.addFiles('lib/moment/moment.js');
-  api.addFiles('lib/moment/locale/en-gb.js', 'client');
-  api.addFiles('lib/moment/locale/pl.js', 'client');
-  api.addFiles('export-moment.js');
+Package.onUse(function(api) {
+    api.export('moment');
+    api.addFiles('moment/moment.js');
+//    api.addFiles('moment/locale/en-gb.js', 'client');
+//    api.addFiles('moment/locale/pl.js', 'client');
 });
-
-if (Package.on_test) {
-  Package.on_test(function (api) {
-    if (Package.onTest) {
-      api.use(['mrt:moment@2.8.1', 'tinytest', 'test-helpers'], ['client', 'server']);
-    } else {
-      api.use(['moment', 'tinytest', 'test-helpers'], ['client', 'server']);
-    }
-    api.add_files('test-mrt:moment.js', ['client', 'server']);
-  });
-}
